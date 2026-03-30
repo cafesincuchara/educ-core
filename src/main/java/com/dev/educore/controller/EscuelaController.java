a66e78e8aec25b06f9651af4585dc749a2b08633
cafesincuchara <vicente.herrera.565656@gmail.com>
2026-03-30 18:49:08 -0300

    feat: creacion de nuevo metodo get para obtner todas las escuelas

--- src/main/java/com/dev/educore/controller/EscuelaController.java
+++ src/main/java/com/dev/educore/controller/EscuelaController.java
@@ -1,4 +1,27 @@
 package com.dev.educore.controller;
 
+import com.dev.educore.model.Escuela;
+import com.dev.educore.service.EscuelaService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/escuelas")
+@AllArgsConstructor
 public class EscuelaController {
+
+    private final EscuelaService service;
+
+    @GetMapping
+    public ResponseEntity<List<Escuela>> ObtenerEscuelas(){
+        return ResponseEntity.ok(service.listarEscuelas());
+    }
+
+
 }
