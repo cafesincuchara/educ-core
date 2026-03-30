b7ea1cb556ea9a92e18b11661f2c4aab7349edd0
cafesincuchara <vicente.herrera.565656@gmail.com>
2026-03-30 14:12:09 -0300

    Se agregan importes

--- src/main/java/com/dev/educore/model/Escuela.java
+++ src/main/java/com/dev/educore/model/Escuela.java
@@ -1,4 +1,24 @@
 package com.dev.educore.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+
+@Entity
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "escuelas")
 public class Escuela {
+
+    @Id
+    private UUID id;
+
 }
